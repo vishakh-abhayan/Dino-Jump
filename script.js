ec989f2f1f2b4dce7f09f38d0d61b535eb9256de
Vishakh Abhayan <vishakhabhayan0@gmail.com>
2022-05-27 22:50:41 +0530

    add function to score

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-let container = document.querySelector("#container")
-let dino = document.querySelector("#dino")
-let block = document.querySelector("#block")
-let road = document.querySelector("#road")
-let score = document.querySelector("#score")
-let gameover = document.querySelector("#gameover")
+let container = document.querySelector("#container");
+let dino = document.querySelector("#dino");
+let block = document.querySelector("#block");
+let road = document.querySelector("#road");
+let Score = document.querySelector("#Score");
+let gameover = document.querySelector("#gameover");
+
+
+//declar varble for score
+
+let interval = null;
+let playScore = 0;
+
+//founctio for score
+
+let scoreCounter = ()=>{
+    playScore++;
+    Score.innerHTML = 'Score <b>${playScore}</b>';
+}
+
+interval = setInterval(scoreCounter,200);
